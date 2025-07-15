d82db10a24aaaf36a4afa827337ace1929c7252f
bdb <emmanuelbadibanga250gmail.com>
2025-07-15 01:27:44 +0100

    build:admin

--- sms/admin.py
+++ sms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from sms.models import SMS, Client, SMSDelivery
+
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(SMS)
+admin.site.register(SMSDelivery)
+
